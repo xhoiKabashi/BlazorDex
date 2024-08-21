0304bfb927a9d93e925c32aa5b5fcd6491a2b415
Xhokabash <xhokabash@gmail.com>
2024-08-21 21:30:06 +0200

    add client

---
+++ wwwroot/scripts/site.js
@@ -0,0 +1,10 @@
+window.triggerAttackAnimation = () => {
+  const heroImage = document.getElementById("heroImage");
+  if (heroImage) {
+    heroImage.classList.add("attack-animation");
+    // Remove the class after animation completes
+    setTimeout(() => {
+      heroImage.classList.remove("attack-animation");
+    }, 500); // Duration should match the CSS animation duration
+  }
+};
