a0568ce53d7f942379836901c579b629038f5b45
Xhokabash <xhokabash@gmail.com>
2024-09-01 15:59:01 +0200

    add full screen mode

--- wwwroot/scripts/site.js
+++ wwwroot/scripts/site.js
@@ -8,4 +8,16 @@ window.triggerAttackAnimation = () => {
     }, 500); // Duration should match the CSS animation duration
   }
 };
-  
+function requestFullScreen() {
+  var element = document.documentElement;
+
+  if (element.requestFullscreen) {
+      element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) { // Firefox
+      element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
+      element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) { // IE/Edge
+      element.msRequestFullscreen();
+  }
+}
