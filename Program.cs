0b3cef62b5b6186aeb02c8578af4845fdd8c46d4
Xhokabash <xhokabash@gmail.com>
2024-09-03 23:23:55 +0200

    fix api

--- Program.cs
+++ Program.cs
@@ -2,21 +2,23 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorDex;
 using BlazorDex.Util;
+using System.Net.Http;
 
- 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HeroClient(new HttpClient { BaseAddress = new Uri("https://kreshnik-api.onrender.com") }));
 
+// Register the root components
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Register HttpClient with a base address
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://kreshnik-api.onrender.com") });
+
+// Register other services
 builder.Services.AddScoped<HeroClient>();
 builder.Services.AddScoped<HeroStateService>();
 builder.Services.AddScoped<GameAnimationService>();
 builder.Services.AddScoped<GameLogic>();
-builder.Services.AddSingleton<HeroStateService>();
-// builder.Services.AddScoped(sp => new HeroClient(new HttpClient { BaseAddress = new Uri("http://localhost:5024/") }));
-
 
 await builder.Build().RunAsync();
+
+// builder.Services.AddScoped(sp => new HeroClient(new HttpClient { BaseAddress = new Uri("http://localhost:5024/") }));
